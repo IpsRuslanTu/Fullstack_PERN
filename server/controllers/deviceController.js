e4877cd6a8388c69e8e582e311c37b8d82b880a4
Ruslan <r1tu@yandex.ru>
2023-02-23 00:59:53 +0300

    начал реализацию контроллеров

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,5 +1,21 @@
+const {Device} = require('../models/models')
+const uuid = require('uuid')
+const path = require('path')
+const ApiError = require('../error/ApiError')
+
 class DeviceController {
-  async create(req, res) {
+  async create(req, res, next) {
+    try {
+      const {name, price, brandId, typeId, info} = req.body
+      const {img} = req.files
+      const fileName = uuid.v4() + '.jpg'
+      img.mv(path.resolve(__dirname, '..', 'static', fileName))
+      const device = await Device.create({name, price, brandId, typeId, img: fileName})
+
+      res.json(device)
+    } catch (e) {
+      next(ApiError.badRequest(e.message))
+    }
 
   }
 
--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -5,11 +5,13 @@ class TypeController {
   async create(req, res) {
     const {name} = req.body
     const type = await Type.create({name})
-    res.status(200).json(type)
+    res.json(type)
   }
 
   async getAll(req, res) {
+    const types = await Type.findAll();
 
+    res.json(types)
   }
 }
 
